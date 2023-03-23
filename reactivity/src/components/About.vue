c8074c3e800c06b62728dcf86b35fbb331c0fafa
jeanl7281 <jeanl.03.2007@gmail.com>
2023-03-23 10:15:36 -0400

    code

---
+++ reactivity/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld :list="list" />
+  <TheWelcome :list="list" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: ["Zee", "Ryan", "Katie"],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ reactivity/src/components/Home.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      student: "Sopie",
+      animals: ["pig", "cow", "ostrich", "dog"],
+      graduated: true,
+      selected: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <h1 v-if="selected === 'A'">Correct Answer</h1>
+  <h1 v-else-if="selected === ''">Select an asnwer</h1>
+  <h1 v-else>"Wrong"</h1>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+  <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>{{ selected }}</span>
+</template>
