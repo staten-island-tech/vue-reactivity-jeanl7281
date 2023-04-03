070edf2db32d8d8bc2d5cd7244aefabba9aaa6ee
jeanl7281 <jeanl.03.2007@gmail.com>
2023-04-03 06:37:08 -0400

    code

--- reactivity/src/App.vue
+++ reactivity/src/App.vue
@@ -1,44 +1,4 @@
 <script setup>
-import TheWelcome from "./components/TheWelcome.vue";
-import Home from "./components/Home.vue";
-import About from "./components/About.vue";
-</script>
-
-<template>
-  <header></header>
-
-  <main>
-    <TheWelcome />
-    <Home />
-    <About />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+import TheWelcome from "./views/Home.vue";
+import ShoppingCart from "./components/ShoppingCart.vue";
+import CreateCards from "./components/CreateCards.vue";
---
+++ reactivity/src/views/Home.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  components: {ShoppingCart, CreateCards},
+  data() {
+    return {
+      animalArray: [
+        { name: "German Shepherd", price: "$2000"},
+        { name: "Bulldog", price: "$2500"},
+        { name: "Labrador Retriever", price: "$1000"},
+        { name: "Golden Retriver", price: "$1000"},
+        { name: "Husky", price: "$1750"},
+        { name: "Pomeranian", price: "$6000"}
+      ],
+    cart: [],
+    inCart: true,
+    };
+  },
+  methods: {
+    push: function (items) {
+      this.cart.push(items);
+      console.log(this.cart);
+    },
+    display: function() {
+      this.inCart === !this.inCart;
+    },
+    removeItems: function (items) {
+      this.cart = this.cart.filter((t) => t !== items);
+      console.log(items);
+    },
+  },
+};
+</script>
+
+
+<template>
+  <div id="parent">
+    <cartButton @click="display()" />
+    <sub v-if="inCart === true">
+      <cart 
+      v-for="items in cart"
+      :key="items"
+      :name="items.name"
+      :price="items.price"
+      @click="removeItems(items)"
+      />
+    </sub>
+  </div>
+</template>
+  
